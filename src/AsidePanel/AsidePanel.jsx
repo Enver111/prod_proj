3ceb62003aa99dec14c5af3256d033fd7e8ed81f
Enver <enver.erfanovich@gmail.com>
2024-08-01 20:05:02 +0300

    Added aside panel

--- src/AsidePanel/AsidePanel.jsx
+++ src/AsidePanel/AsidePanel.jsx
@@ -1,5 +1,68 @@
+import React from "react";
+import s from "./AsidePanel.module.css";
+import Button from "../Components/Button";
+
 const AsidePanel = () => {
-  return <aside>Aside</aside>;
+  return (
+    <aside className={s.asidePanel}>
+      <Button
+        size="16"
+        variant="primary"
+        text="Все товары"
+        width={300}
+        height={50}
+        radius="10px"
+      />
+      <Button
+        size="16"
+        variant="primary"
+        text="Моя страница"
+        width={300}
+        height={50}
+        radius="10px"
+      />
+      <Button
+        size="16"
+        variant="primary"
+        text="Сообщения  "
+        width={300}
+        height={50}
+        radius="10px"
+      />
+      <Button
+        size="16"
+        variant="primary"
+        text="Избранное "
+        width={300}
+        height={50}
+        radius="10px"
+      />
+      <Button
+        size="16"
+        variant="primary"
+        text="Мои объявления"
+        width={300}
+        height={50}
+        radius="10px"
+      />
+      <Button
+        size="16"
+        variant="primary"
+        text="Статистика "
+        width={300}
+        height={50}
+        radius="10px"
+      />
+      <Button
+        size="16"
+        variant="primary"
+        text="Настройки"
+        width={300}
+        height={50}
+        radius="10px"
+      />
+    </aside>
+  );
 };
 
 export default AsidePanel;
