740a789ff82949e57357e8a5348a50051d602edd
Enver <enver.erfanovich@gmail.com>
2024-08-01 20:07:20 +0300

    Made changes to the button component

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,35 +3,66 @@ import s from "./Style.module.css";
 
 const Button = ({
   size,
+  backgroundColor,
   variant,
   text,
-  icon,
+  icon1,
+  icon2,
   width,
   height,
   border,
   radius,
+  hoverBackgroundColor,
+  hoverColor,
+  onClick,
 }) => {
   const buttonStyle = {
     cursor: "pointer",
     fontSize: size ? `${size}px` : "14px",
+    backgroundColor: backgroundColor ? `${backgroundColor}` : "rgb(35, 61, 74)",
     color: variant === "primary" ? "white" : "black",
     width: width ? `${width}px` : "auto",
     height: height ? `${height}px` : "auto",
     border: border ? `${border}` : "none",
     borderRadius: radius ? `${radius}` : "unset",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+
+    transition: "background-color 0.3s, color 0.3s",
   };
 
-  const iconStyle = {
-    marginRight: "6px",
+  const handleMouseEnter = (e) => {
+    if (hoverBackgroundColor) {
+      e.target.style.backgroundColor = hoverBackgroundColor;
+    }
+    if (hoverColor) {
+      e.target.style.color = hoverColor;
+    }
   };
+
+  const handleMouseLeave = (e) => {
+    e.target.style.backgroundColor = backgroundColor
+      ? backgroundColor
+      : "rgb(35, 61, 74)";
+    e.target.style.color = variant === "primary" ? "white" : "black";
+  };
+
+  // const iconStyle1 = {
+  //   marginRight: "6px",
+  // };
+  // const iconStyle2 = {
+  //   marginLeft: "20px",
+  // };
 
   return (
-    <button className={s.btn} style={buttonStyle}>
-      {icon && <img src={icon} alt="icon" style={iconStyle} />}
+    <button
+      className={s.btn}
+      style={buttonStyle}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={onClick}
+    >
+      {icon1 && <img src={icon1} alt="icon" />}
       <span className="btn-text">{text}</span>
+      {icon2 && <img src={icon2} alt="icon" />}
     </button>
   );
 };
